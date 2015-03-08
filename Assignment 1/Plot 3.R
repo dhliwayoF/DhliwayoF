62f516e927fe97ef633fe929deb6a85fc94d726a
Fordi Dhliwayo <fordi@live.co.za>
2015-03-08 23:34:17 +0200

    Assignment 1
    Exploratory Data Analysis

--- Assignment 1/Plot 3.R
+++ Assignment 1/Plot 3.R
@@ -7,8 +7,10 @@ house<- subset(house,as.Date(house$Date,format='%d/%m/%Y')<=as.Date('2007-02-02'
 png(filename="house$Sub_metering_1")
 par(mfrow= c(1,1))
 plot(house$Sub_metering_1,xlab="DateTime",ylab="Sub metering",type="n")
-lines(house$Sub_metering_1,type="l", xlab="DateTime",ylab="Sub metering")
+lines(house$Sub_metering_1,type="l", xlab="DateTime",ylab="Sub metering",col="black")
 lines(house$Sub_metering_2,type="l", xlab="DateTime",ylab="Sub metering", col="red")
 lines(house$Sub_metering_3,type="l", xlab="DateTime",ylab="sub metering", col="blue")
-legend("topright",legend=c("Sub_metering_1","sub_metering_2","Sub_metering_3,))
-dev.off()                          
+legend("topright",legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),lty=c(1,1),lwd=c(2.5,2.5),col=c("black", "red","blue"))
+dev.off()
+
+
